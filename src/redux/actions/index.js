6516979f6d4b44ae188b506ab672bc1bbbe206c1
fkrein1 <felipekrein@gmail.com>
2022-05-24 15:35:07 -0300

    cria estrutura do redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+// export const addTrip = (trip) => ({
+//   type: 'ADD_TRIP',
+//   payload: trip,
+// });
+
+// const addCurrencies = (currencies) => ({
+//   type: 'ADD_CURRENCIES',
+//   payload: currencies,
+// });
