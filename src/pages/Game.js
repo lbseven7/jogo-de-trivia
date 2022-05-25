9ecb22be788e07caaf5a032911079505f2ea8d9b
jean <jeansantos@id.uff.br>
2022-05-25 17:27:19 -0300

    início do requisito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getAsk from '../services/getAsk';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      trivia: [],
+      index: 0,
+    };
+  }
+
+  async componentDidMount() {
+    const { history } = this.props;
+    const token = localStorage.getItem('token');
+    if (token === null) history.push('/');
+    const API_ASK = await getAsk(token);
+    const tokenInvalid = 3;
+    if (API_ASK.response_code === tokenInvalid) {
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+    const tokenValid = 0;
+    if (API_ASK.response_code === tokenValid) {
+      this.setState({ trivia: API_ASK.results });
+    }
+  }
+
   render() {
+    const { trivia, index } = this.state;
     return (
-      <h1>Game</h1> // não excluir, usa para teste
+      <div>
+        <h1>Game</h1>
+        { trivia.length !== 0 && (
+          <div>
+            <p data-testid="question-text">trivia[index].question</p>
+            <p data-testid="question-category">
+              {trivia[index].category}
+            </p>
+          </div>
+
+        ) }
+
+      </div>
+
     );
   }
 }
-
+Game.propTypes = {
+  history: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
 export default Game;
