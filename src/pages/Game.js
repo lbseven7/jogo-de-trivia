ebc68f0dc1c9e332f9064beef7f5ffe5474cb139
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-05-26 17:42:31 -0300

    Requisito 07 e 08 parcial

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getAsk from '../services/getAsk';
 import Header from '../component/Header';
+import '../App.css';
 
 class Game extends React.Component {
   constructor() {
@@ -9,6 +10,10 @@ class Game extends React.Component {
     this.state = {
       trivia: [],
       index: 0,
+      answeredQuestion: false,
+      timer: 30,
+      random: false;
+      randomPosition,
     };
   }
 
@@ -26,40 +31,74 @@ class Game extends React.Component {
     if (API_ASK.response_code === tokenValid) {
       this.setState({ trivia: API_ASK.results });
     }
+    this.setTimer();
+    this.randomPosition()
   }
 
-  answers() {
-    const { trivia, index } = this.state;
-    const max = trivia[index].incorrect_answers.length;
-    const correct = Math.floor(Math.random() * (max - 0 + 1) + 0);
+  setTimer = () => {
+    const { timer } = this.state;
+    const oneSecond = 1000;
+    const myInterval = setInterval(() => {
+      this.setState((prevState) => ({
+        timer: prevState.timer - 1,
+      }), (myInterval) => if (timer === 0) clearInterval(myInterval));
+    }, oneSecond);
+  }
+
+  randomPosition = () => {
+    const { random, trivia } = this.state
+    if (!random && trivia.lenght > 0) {
+      const max = trivia[index].incorrect_answers.length;
+      const randomPosition = Math.floor(Math.random() * (max - 0 + 1) + 0);
+      this.setState({ randomPosition })
+    }
+  }
+
+  answers = () => {
+    const { trivia, index, answeredQuestion, randomPosition } = this.state;
+    const multiple = 4;
+    const totalAnswers = trivia[index].type === 'boolean' ? 2 : multiple;
     const answers = trivia[index].incorrect_answers;
-    answers.splice(correct, 0, trivia[index].correct_answer);
+    if (answers.length < totalAnswers) {
+      answers.splice(randomPosition, 0, trivia[index].correct_answer);
+    }
     return (
-      answers.map((answer, i) => (
-        <button
-          key={ i }
-          type="button"
-          data-testid={ this.dataTestAnswer(i, correct) }
-        >
-          { answer }
-        </button>
-      ))
+      answers
+        .map((answer, i) => (
+          <button
+            key={ i }
+            type="button"
+            disabled={ answeredQuestion }
+            data-testid={ this.dataTestAnswer(i, randomPosition) }
+            onClick={ () => this.setState({ answeredQuestion: true }) }
+            className={ this.btnStyle(i, randomPosition) }
+          >
+            { answer }
+          </button>
+        ))
     );
   }
 
-  dataTestAnswer(index, correct) {
+  dataTestAnswer = (index, correct) => {
     if (index === correct) {
       return 'correct-answer';
     } if (index > correct) {
-      return `wrong-answer-${i - 1}`;
-    } return `wrong-answer-${i}`;
+      return `wrong-answer-${index - 1}`;
+    } return `wrong-answer-${index}`;
+  }
+
+  btnStyle = (index, correct) => {
+    const { answeredQuestion } = this.state;
+    if (answeredQuestion && index === correct) return 'correct-btn';
+    if (answeredQuestion && index !== correct) return 'incorrect-btn';
   }
 
   render() {
-    const { trivia, index } = this.state;
+    const { trivia, index, timer } = this.state;
     return (
       <div>
         <Header />
+        <p>{ timer }</p>
         { trivia.length !== 0 && (
           <div>
             <p data-testid="question-text">{ trivia[index].question }</p>
